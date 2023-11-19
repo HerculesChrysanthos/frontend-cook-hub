297074f3abbc406c5dbcaf9cfec154bda3400e5f
despknkd <des.kanakidou@gmail.com>
2023-11-19 21:58:23 +0200

    first attempt navigation bar

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    console.log(`Logging in with username: ${username}, password: ${password}`);
+  };
+
+  return (
+    <div>
+      <form>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Logo from "../../images/Logo.png";
+import { Link } from "react-router-dom"; //best practice to use this component instead of link tag
+import "../../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='leftSide'>
+          <img src={Logo} />
+          <Link to='/Login'> Login </Link>
+          <Link to='/Register'> Register </Link>
+      </div>
+      <div className='rightSide'>
+        <Link to='/'> Home </Link>
+        <Link to="/menu"> Recipes </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
+
