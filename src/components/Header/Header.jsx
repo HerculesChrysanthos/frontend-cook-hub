0334214fe52ba6a8d06c2ebea762e287e5df3e9c
kostassifa <sifavs@yahoo.com>
2023-11-21 19:33:46 +0200

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,23 @@
 // Header.js
 import React from 'react';
+import logoImage from '../../images/Group 2.svg';
+
 
 const Header = () => {
   return (
     <header>
+       <div className="logo-container">
+          <img src={logoImage} alt="Logo" />
+        </div>
       <nav>
-        <ul>
+        <ul className="header-nav">
           <li><a href="/">Home</a></li>
-          <li><a href="/search">Search</a></li>
+          <li><a href="/">Recipes</a></li>
+          <li><a href="/">Category</a></li>
+          <li className="magnify"><a href="/search">Search</a></li>
           <li><a href="/login">Login</a></li>
-        </ul>
+          <li><a href="/login">Register</a></li>
+        </ul>   
       </nav>
     </header>
   );
