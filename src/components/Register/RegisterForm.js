935a4d6bff56a815842605470470a517be5c78a7
kostassifa <sifavs@yahoo.com>
2023-11-26 23:44:42 +0200

    register

--- src/components/Register/RegisterForm.js
+++ src/components/Register/RegisterForm.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 
-
 const RegisterForm = ({ onSubmit, loading, error, successMessage }) => {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
 
   return (
     <div className="register-container">
       <h1>User Registration</h1>
       {error && <div className="error">{error}</div>}
       {successMessage && <div className="success">{successMessage}</div>}
-      <form onSubmit={onSubmit}>
+      <form onSubmit={(e) => onSubmit(e, { name, surname, email, password, passwordConfirmation })}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+        </label>
+        <label>
+          Surname:
+          <input type="text" value={surname} onChange={(e) => setSurname(e.target.value)}  required  />
+        </label>
         <label>
           Email:
           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
@@ -21,6 +31,15 @@ const RegisterForm = ({ onSubmit, loading, error, successMessage }) => {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Confirm Password:
+          <input
+            type="password"
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
             required
           />
         </label>
--- src/components/Register/Register_handling.js
+++ src/components/Register/Register_handling.js
@@ -7,36 +7,71 @@ const Register_handling = () => {
   const [error, setError] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
 
-  // Define email and password state variables
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleRegister = async (e) => {
+  const handleRegister = async (e, formData) => {
     e.preventDefault();
+  
+    const { name, surname, email, password, passwordConfirmation } = formData;
 
+        // Define headers
+        const headers = {
+          'Content-Type': 'application/json',
+          'Accept': '*/*',
+         'Cache-Control': 'no-cache',      
+         'Accept-Encoding': 'gzip, deflate, br',
+         'Connection': 'keep-alive'
+        };
+    
     try {
       setLoading(true);
       setError(null);
-
+  
+      console.log('Sending request with data:', formData);
+  
       // Perform the registration request
-      const response = await axios.post('https://combative-jumper-pig.cyclic.app/api/users/register', {
-        email,
-        password,
-      });
+      const response = await axios.post(
+        '/api/users/register',
+        {
+          name,
+          surname,
+          email,
+          password,
+          passwordConfirmation
+        },
+        headers
+      );
 
+      console.log(response);
+  
       // Registration success
       setSuccessMessage(response.data.message);
     } catch (error) {
       // Registration error
-      setError(error.response ? error.response.data.message : 'An error occurred');
-
+      console.error('Registration error:', error);
+  
+      if (error.response) {
+        const statusCode = error.response.status;
+        console.log('HTTP status code:', statusCode);
+    
+        if (statusCode === 409) {
+          setError("Email already exists");
+        } else if (statusCode === 422) {
+          setError("Password does not match");
+        } else {
+          // For other errors, use a generic message
+          setError(error.response.data.message || 'An error occurred');
+        }
+      } else {
+        // Handle non-response errors
+        setError('An error occurred');
+      }
+    
       // Rethrow the error to handle it at the calling site
-      throw error;
+    //  throw error;
     } finally {
       setLoading(false);
     }
   };
-
+  
   return (
     <RegisterForm
       onSubmit={handleRegister}
