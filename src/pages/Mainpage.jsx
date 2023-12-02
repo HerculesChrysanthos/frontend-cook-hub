32e5b0d88ac15bccf70332f43a12fa77e828aae5
kostassifa <sifavs@yahoo.com>
2023-12-02 19:59:38 +0200

    Card wrapper

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import ImageSlider from '../components/ImageSlider/ImageSlider';
+import card_image from '../images/image.jpg';
+import Card from '../components/Card/Card'; // Import your Card component
 
 
 const MainPage = () => {
@@ -8,6 +10,14 @@ const MainPage = () => {
     <div>
       <Header />
       <ImageSlider />
+       {/* Card Columns Wrapper */}
+       <div className="card-columns-wrapper">
+            <Card
+              title="Example Title"
+              description="This is an example description."
+              card_image={card_image}
+            />
+        </div>
     </div>     
   );
 };
