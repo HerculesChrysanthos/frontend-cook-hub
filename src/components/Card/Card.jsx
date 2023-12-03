c3766138fc14d2203e8792754bdceef966278f42
kostassifa <sifavs@yahoo.com>
2023-12-03 14:28:21 +0200

    card 2

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+// Card.jsx
+import React, { useState } from 'react';
 
+const Card = ({ card_image, title, description }) => {
+  const [isHovered, setIsHovered] = useState(false);
 
-const Card = ({ card_image,title, description }) => {
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <div className="card">
-      <img src={card_image} className="card-img-top" alt="Card" />
+    <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className={`image-container ${isHovered ? 'hovered' : ''}`}>
+        <img src={card_image} className="card-img-top" alt="Card" />
+      </div>
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{description}</p>
--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import ImageSlider from '../components/ImageSlider/ImageSlider';
 import card_image from '../images/image.jpg';
+import card_image_2 from '../images/image_1.jpg';
 import Card from '../components/Card/Card'; // Import your Card component
 
 
@@ -10,12 +11,20 @@ const MainPage = () => {
     <div>
       <Header />
       <ImageSlider />
+      <br/>
+      {/* Recipes Section Header */}
+      <h2>Recipes</h2>
        {/* Card Columns Wrapper */}
        <div className="card-columns-wrapper">
             <Card
               title="Example Title"
               description="This is an example description."
               card_image={card_image}
+            />
+             <Card
+              title="Example Title_2"
+              description="This is an example description_2."
+              card_image={card_image_2}
             />
         </div>
     </div>     
