4e5c7216a5916d6f5c358225f047e3cf223d80bf
kostassifa <sifavs@yahoo.com>
2023-12-12 14:37:08 +0200

    cATEGORIES with sub hoover

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -5,11 +5,12 @@ import axios from "axios";
 const Categories = ({ handleCategoryClick }) => {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [showSubcategories, setShowSubcategories] = useState(null);
 
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await axios.get("/api/categories");
+        const response = await axios.get("/api/categories/?include=subcategories");
         setCategories(response.data);
       } catch (error) {
         console.error("Error fetching categories:", error);
@@ -21,23 +22,43 @@ const Categories = ({ handleCategoryClick }) => {
     fetchCategories();
   }, []);
 
+  const handleMouseEnter = (index) => {
+    setShowSubcategories(index);
+  };
+
+  const handleMouseLeave = () => {
+    setShowSubcategories(null);
+  };
+
   return (
-    <div className="categories-container">
+    <div className="categories-container" onMouseLeave={handleMouseLeave}>
       <div className="categories-dropdown">
         {loading ? (
           <div>Loading categories...</div>
         ) : (
           <ul>
-            {categories.map((category) => (
-              <li key={category.id} onClick={() => handleCategoryClick(category)}>
-                <span style={{ display: 'block' }}>{category.name}</span>
+            {categories.map((category, index) => (
+              <li
+                key={category.category._id}
+                onMouseEnter={() => handleMouseEnter(index)}
+              >
+                <span style={{ display: 'block' }}>{category.category.name}</span>
+                {showSubcategories === index && category.subcategories && category.subcategories.length > 0 && (
+                  <ul>
+                    {category.subcategories.map((subcategory) => (
+                      <li key={subcategory._id} onClick={() => handleCategoryClick(subcategory)}>
+                        <span style={{ display: 'block' }}>{subcategory.name}</span>
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </li>
             ))}
           </ul>
         )}
       </div>
     </div>
-  );  
+  );
 };
 
 export default Categories;
