63a06f82038fd3f97b589036fd484b3bc03d93f5
despknkd <des.kanakidou@gmail.com>
2023-12-16 01:48:05 +0200

    add navigation to specific recipe

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,13 +1,21 @@
 // RecipeCard.js
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const RecipeCard = ({ recipe }) => {
+  const navigate = useNavigate();
+
+  const handleRecipeClick = () => {
+    // Redirect to the recipe page
+    console.log("Selected recipe:", recipe.name);
+    navigate(`/recipes/${recipe._id}`);
+  }
+
   return (
-    <div className="recipe-card">
+    <div className="recipe-card" onClick={handleRecipeClick} style={{ cursor: 'pointer' }}>
       <h2>{recipe.title}</h2>
       <img src={recipe.mainImage} alt={recipe.title} className="recipe-image" />
       <p>{recipe.description}</p>
-      {/* Add more details as needed */}
     </div>
   );
 };
