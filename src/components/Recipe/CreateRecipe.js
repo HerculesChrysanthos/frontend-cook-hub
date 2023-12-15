5fdb58419412af12e9c084362082009d34ced46d
despknkd <des.kanakidou@gmail.com>
2023-12-15 16:31:20 +0200

    create recipe modification

--- src/components/Recipe/CreateRecipe.js
+++ src/components/Recipe/CreateRecipe.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useGetUserID } from "../../Hooks/useGetUserID";
 import { useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie";
+import { useEffect } from "react";
 
 const CreateRecipe = () => {
   const userID = useGetUserID();
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   const [recipe, setRecipe] = useState({
     title: "",
     description: "",
@@ -15,15 +15,42 @@ const CreateRecipe = () => {
     imageUrl: "",
     preparationTime: 0,
     cookingTime: 0,
-    categories: [],
-    subcategories: [],
-    userOwner: userID,
+    category: "",
+    subcategory: "",
+    // user: userID,
+    tags: [],
   });
+
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
+
+  useEffect(() => {
+    // Retrieve categories from local storage on component mount
+    const storedCategories = localStorage.getItem("categories");
+    if (storedCategories) {
+      // Parse the stored JSON data
+      const parsedCategories = JSON.parse(storedCategories);
+      console.log("parsedCategories:", parsedCategories);
+      console.log("json:", storedCategories);
+      // Set the retrieved categories to state
+      setCategories(parsedCategories);
+    }
+  }, []);
 
   const navigate = useNavigate();
 
   const handleChange = (event) => {
     const { name, value } = event.target;
+    if (name === "category") {
+      // Update subcategories based on the selected category
+      const selectedCategory = categories.find(
+        (cat) => cat.category.name === value
+      );
+      const selectedSubcategories = selectedCategory
+        ? selectedCategory.subcategories
+        : [];
+      setSubcategories(selectedSubcategories);
+    }
     setRecipe({ ...recipe, [name]: value });
   };
 
@@ -37,6 +64,18 @@ const CreateRecipe = () => {
   const handleAddIngredient = () => {
     const ingredients = [...recipe.ingredients, ""];
     setRecipe({ ...recipe, ingredients });
+  };
+
+  const handleTagChange = (event, index) => {
+    const { value } = event.target;
+    const tags = [...recipe.tags];
+    tags[index] = value;
+    setRecipe({ ...recipe, tags });
+  };
+
+  const handleAddTag = () => {
+    const tags = [...recipe.tags, ""];
+    setRecipe({ ...recipe, tags });
   };
 
   const handleSubmit = async (event) => {
@@ -101,15 +140,15 @@ const CreateRecipe = () => {
           type="file"
           id="image"
           name="image"
-          accept="image/jpeg"
+          accept="image/jpeg, image/png"
           required
         />
-        <label htmlFor="cookingTime">Χρόνος Προετοιμασίας (λεπτά)</label>
+        <label htmlFor="preparationTime">Χρόνος Προετοιμασίας (λεπτά)</label>
         <input
           type="number"
           id="preparationTime"
           name="preparationTime"
-          value={recipe.cookingTime}
+          value={recipe.preparationTime}
           onChange={handleChange}
         />
         <label htmlFor="cookingTime">Χρόνος Μαγειρέματος (λεπτά)</label>
@@ -120,6 +159,55 @@ const CreateRecipe = () => {
           value={recipe.cookingTime}
           onChange={handleChange}
         />
+        <label htmlFor="servings">Μερίδες (λεπτά)</label>
+        <input
+          type="number"
+          id="servings"
+          name="servings"
+          value={recipe.servings}
+          onChange={handleChange}
+        />
+        <label htmlFor="category">Κατηγορία</label>
+        <select
+          id="category"
+          name="category"
+          value={recipe.category}
+          onChange={handleChange}
+        >
+          <option value="">Διάλεξε Κατηγορία</option>
+          {categories.map((category) => (
+            <option key={category.category._id} value={category.category.name}>
+              {category.category.name}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="subcategory">Υποκατηγορία</label>
+        <select
+          id="subcategory"
+          name="subcategory"
+          value={recipe.subcategory}
+          onChange={handleChange}
+        >
+          <option value="">Διάλεξε Υποκατηγορία</option>
+          {subcategories.map((subcategory) => (
+            <option key={subcategory._id} value={subcategory.name}>
+              {subcategory.name}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="tags">Tags</label>
+        {recipe.tags.map((tag, index) => (
+          <input
+            key={index}
+            type="text"
+            name="tags"
+            value={tag}
+            onChange={(event) => handleTagChange(event, index)}
+          />
+        ))}
+        <button type="button" onClick={handleAddTag}>
+          Προσθήκη Tag
+        </button>
         <button type="submit">Υποβολή</button>
       </form>
     </div>
